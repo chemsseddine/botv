6fcc8df89eef328d16c5bd4caf94d7fa3b3ef4ee
Chemss Eddine Ben Hassine <chamseddine.benhasine@vneuron.tn>
2018-12-08 14:15:53 +0100

    Move to v1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'botv',         # How you named your package folder (MyLib)
   packages = ['botv'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Easy to use dict constructor for botv.io usage',   # Give a short description about your library
   author = 'Chemss Eddine Ben Hassine',                   # Type in your name
   author_email = 'bhchemss@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/chemsseddine/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/chemsseddine/botv/archive/v0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/chemsseddine/botv/archive/v1.0.tar.gz',    # I explain this later on
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
