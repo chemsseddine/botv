7d3948fa81dcaa95ded392d60bdf6bdcef7ecc6c
Chemss Eddine Ben Hassine <chamseddine.benhasine@vneuron.tn>
2018-12-12 21:06:01 +0100

    Update Pypi Docs

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name = 'botv',
   packages = ['botv'],
-  version = '2.2',
+  version = '2.3',
   license='MIT',
   description = 'Easy to use dict constructor for botv.io usage',
+  long_description = long_description,
   author = 'Chemss Eddine Ben Hassine',
   author_email = 'bhchemss@gmail.com',
   url = 'https://github.com/chemsseddine/',
-  download_url = 'https://github.com/chemsseddine/botv/archive/v2.2.tar.gz',
+  download_url = 'https://github.com/chemsseddine/botv/archive/v2.3.tar.gz',
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
